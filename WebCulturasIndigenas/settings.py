06c33e49532b95677a6b97fd35abec50d7e9befc
ChekeGT <chekelosos@gmail.com>
2018-09-14 22:19:27 -0500

    Solucionado error con la libreria photostack
    El error producia que no se agregase al repositorio
    la carpeta con los archivos de photostack, solucionado
    cambiando la carpeta de lugar; un poco raro lo se
    pero funciona :v

--- WebCulturasIndigenas/settings.py
+++ WebCulturasIndigenas/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'translate',
     'indigenous_culture',
-    'core',
 ]
 
 MIDDLEWARE = [
